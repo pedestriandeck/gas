17af4ca365a317f160868690c6c8ec412de9c0b2
pedestriandeck <153702277+pedestriandeck@users.noreply.github.com>
2024-02-14 08:03:35 +0000

    bbb

--- gas_files/css/common_post.js
+++ gas_files/css/common_post.js
@@ -1,1 +1,35 @@
 /* POST共通 */
+/* ===== クエリストリング取得・設定 ===== */
+// クエリストリングから指定したkey項目の値を取得する
+function getQueryParams(key) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(key);
+}
+
+/* URLにクエリストリングを追加して返却する
+    @url : "./example.html"
+    @queryParams : {query1: "aaa",query2: "bbb"}
+*/
+function setQueryParams(url, queryParams) {
+    // 相対パスを絶対パスに変換
+    let path = document.createElement("a");
+    path.href = url;
+
+    // URLオブジェクトを作成する
+    const urlObj = new URL(path.href);
+
+    // URLSearchParamsオブジェクトを取得する
+    const urlSearchParams = urlObj.searchParams;
+
+    // クエリストリングのオブジェクトをループで処理する
+    for (const [key, value] of Object.entries(queryParams)) {
+        // クエリストリングのキーと値をURLSearchParamsオブジェクトに追加または更新する
+        urlSearchParams.set(key, value);
+    }
+
+    // URLオブジェクトのsearchプロパティを元の相対パスに結合して返却する
+    const returnPath = url + "?" + urlSearchParams.toString();
+
+    // 相対パスを返す
+    return returnPath;
+}
--- gas_files/css/post01.js
+++ gas_files/css/post01.js
@@ -1,36 +1,38 @@
 /* POST01 */
 window.addEventListener('DOMContentLoaded', function () {
-    const id = document.getElementById('po1_name');
+    const name = document.getElementById('po1_name');
     const pw = document.getElementById('po1_pw');
     const confirmBtn = document.getElementById('po1_confirmBtn');
 
-    confirmBtn.addEventListener('click', function() {
+    confirmBtn.addEventListener('click', function () {
         const webAplUrl = 'https://script.google.com/macros/s/AKfycbzlD3R49PX4u4ypzqSWEaQTOikCfDaJb0AjR3isafY8l554OlTepn_PNIqqB8-F2XmRMg/exec';
-        
+
         const sendData = {
-            "id": id.value,
+            "name": name.value,
             "password": pw.value
         };
 
         const postRaram = {
-            "method"     : "POST",
-            "mode"       : "no-cors",
-            "Content-Type" : "application/x-www-form-urlencoded",
-            "body" : JSON.stringify(sendData)
+            "method": "POST",
+            "mode": "no-cors",
+            "Content-Type": "application/json",
+            "body": JSON.stringify(sendData)
         };
 
-        UrlFetchApp.fetch(webAplUrl).then(response => {
+        fetch(webAplUrl, postRaram).then((response) => {
             if (response.ok) {
                 console.log('response: ok');
             } else {
                 console.log('response: nooooo!!');
             }
             return response.json();
-        }).then(data => {
-            console.log('data.status: ' + data.status);
-            console.log('data.id: ' + data.id);
-        }).catch(error => {
+        }).then((data) => {
+            console.log('data.status: ' + data.status + '\ndata.id: ' + data.id);
+            window.alert('data.status: ' + data.status + '\ndata.id: ' + data.id);
+            location.href = setQueryParams('./post02.html', { "id": data.id });
+        }).catch((error) => {
             console.log(error);
+            window.alert(error);
         });
     });
 });
